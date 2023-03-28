b2f80bfb55a1f0fb57edea8ebd18fed1670cad4a
giospes <giovannijacopo.spes@gmail.com>
2023-03-28 14:59:38 +0200

    done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			message: 'Ciao Signori'
+			message: 'Ciao Signori',
+            imgSrc: 'https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTc0MzMxNjUzNjM0NzI5NjA2/donald-trump-gettyimages-687193180.jpg'
 		}
 	}
 });
